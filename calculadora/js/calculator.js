4b23e377aa5f76be772c930b1dd5c90e4386777f
cavillaz <88896355+cavillaz@users.noreply.github.com>
2024-04-10 20:59:48 -0500

    Delete calculadora directory

--- calculadora/js/calculator.js
+++
@@ -1,8 +0,0 @@
-class Calculator{
-    basic_operar(num1, num2, operador){
-        this.solucion = (num1+operador+num2);
-        this.numPantalla = eval(this.solucion);
-        return this.numPantalla;
-    }
-
-}
